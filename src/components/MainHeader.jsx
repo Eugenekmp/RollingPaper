c8695959f24415d4845c874844120520f65680af
김윰 <kimyumin71@gmail.com>
2026-03-06 00:32:50 +0900

    feat: 로고 및 페이지 이동 버튼 컴포넌트 구현

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,14 +1,22 @@
+/**
+ 서비스의 공통 헤더 컴포넌트
+ - 모든 페이지: 로고 표시
+ - 메인 페이지('/')에서만: '롤링 페이퍼 만들기' 버튼 표시
+ **/
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Logo from './Logo';
 import CreateButton from './CreateButton';
-import ExploreButton from './ExploreButton'; 
 
 const MainHeader = () => {
+  const location = useLocation();
+  
+  const isMainPage = location.pathname === '/';
+
   return (
     <header>
       <Logo />
-      <CreateButton />
-      <ExploreButton /> 
+      {isMainPage && <CreateButton />}
     </header>
   );
 };
