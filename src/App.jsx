df38967b67b207138a38013ed0e6fa86dd6b6d4c
chaemin <sulv0309@gmail.com>
2026-03-06 17:24:56 +0900

    feat: 상세 페이지 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainHeader from './components/MainHeader';
-import RollingPaperDetail from './pages/RollingPaperDetail';
 import MainPage from './pages/MainPage';
 import Post from './pages/Post';
 import ListPage from './pages/ListPage';
+import DetailPage from './pages/DetailPage';
 
 const App = () => {
   return (
-    <Router>
+    <>
       <MainHeader /> 
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/post" element={<Post />} />
-        <Route path="/post/:id" element={<RollingPaperDetail />} />
+        <Route path="/post/:id" element={<DetailPage />} />
         <Route path="/list" element={<ListPage />} />        
       </Routes>
-    </Router>
+    </>
   );
 };
 
