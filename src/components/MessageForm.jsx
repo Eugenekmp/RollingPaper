335e6ffec3c232aff4ffbf9134fb2bd9ace918e0
eugene <eugenekmpark@gmail.com>
2026-03-09 15:59:08 +0900

    refactor: selection 컴포넌트 순서 수정

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -13,7 +13,7 @@ function MessageForm() {
   const [profileImageURL, setProfileImageURL] = useState(
     "https://fastly.picsum.photos/id/311/200/200.jpg?hmac=CHiYGYQ3Xpesshw5eYWH7U0Kyl9zMTZLQuRDU4OtyH8",
   );
-  const [relationship, setRelationship] = useState("친구");
+  const [relationship, setRelationship] = useState("지인");
   const [content, setContent] = useState("");
   const [font, setFont] = useState("Noto Sans");
   const [isLoading, setIsLoading] = useState(false);
