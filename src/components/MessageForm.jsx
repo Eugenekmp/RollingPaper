76ddd3e3217862ec67d1056b484e898a990de728
kujihye <gji135@naver.com>
2026-03-11 17:14:50 +0900

    feat: 메세지 생성하기 페이지에 토스트 추가

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -7,17 +7,21 @@ import { useNavigate, useParams } from "react-router-dom";
 import InputForm from "./InputForm";
 import ProfileImgContainer from "./ProfileImgContainer";
 import styled from "styled-components";
+import MyContext from "../components/MyContext";
+import ToastBox from "../components/ToastBox";
 
 function MessageForm() {
   const { id } = useParams();
   const [sender, setSender] = useState("");
   const [profileImageURL, setProfileImageURL] = useState(
-    "https://learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com/sprint-proj-image/default_avatar.png",
+    "https://learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com/sprint-proj-image/default_avatar.png"
   );
   const [relationship, setRelationship] = useState("지인");
   const [content, setContent] = useState("");
   const [font, setFont] = useState("Noto Sans");
   const navigate = useNavigate();
+  const [shouldShowToastMessage, setShouldShowToastMessage] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
 
   //필수 입력값이 없으면 버튼 비활성화
   const isDisabled = !sender.trim() || content === "<p><br></p>" || !content;
@@ -29,45 +33,65 @@ function MessageForm() {
     try {
       await axios.post(
         `https://rolling-api.vercel.app/23-5/recipients/${id}/messages/`,
-        { sender, profileImageURL, relationship, content, font },
+        { sender, profileImageURL, relationship, content, font }
       );
       navigate(`/post/${id}`);
     } catch {
-      alert("메세지 작성에 실패하였습니다");
+      makeToast("메세지 작성에 실패하였습니다");
     }
   };
 
+  const makeToast = (message) => {
+    setShouldShowToastMessage(true);
+    setToastMessage(message);
+  };
+
   return (
-    <Container>
-      <InputForm
-        onChange={setSender}
-        label="FROM"
-        placeholder="이름을 입력해주세요"
-        value={sender}
-      />
+    <MyContext.Provider
+      value={{
+        shouldShowToastMessage,
+        setShouldShowToastMessage,
+        makeToast,
+      }}
+    >
+      <Container>
+        <InputForm
+          onChange={setSender}
+          label="FROM"
+          placeholder="이름을 입력해주세요"
+          value={sender}
+        />
 
-      <ProfileImgContainer
-        label="프로필 이미지"
-        value={profileImageURL}
-        changeImg={setProfileImageURL}
-      />
+        <ProfileImgContainer
+          label="프로필 이미지"
+          value={profileImageURL}
+          changeImg={setProfileImageURL}
+        />
 
-      <Selection
-        value={relationship}
-        onChange={setRelationship}
-        type={"relation"}
-      >
-        상대와의 관계
-      </Selection>
+        <Selection
+          value={relationship}
+          onChange={setRelationship}
+          type={"relation"}
+        >
+          상대와의 관계
+        </Selection>
 
-      <TextEditor onChange={setContent} font={font} />
+        <TextEditor onChange={setContent} font={font} />
 
-      <Selection value={font} onChange={setFont} type={"font"}>
-        폰트 선택
-      </Selection>
+        <Selection value={font} onChange={setFont} type={"font"}>
+          폰트 선택
+        </Selection>
 
-      <Button onClick={handleSubmit} disabled={isDisabled} />
-    </Container>
+        <Button onClick={handleSubmit} disabled={isDisabled} />
+
+        {shouldShowToastMessage && (
+          <ToastBox
+            toastMessage={toastMessage}
+            setShowToastMessage={setShouldShowToastMessage}
+          />
+        )}
+      </Container>
+    </MyContext.Provider>
   );
 }
 
