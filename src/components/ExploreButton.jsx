4e7d699f490cc5870e7657d18964a73cdc2b5cea
김윰 <kimyumin71@gmail.com>
2026-03-12 14:54:14 +0900

    feat: 메인 페이지 및 헤더 피그마 수치 반영

--- src/components/ExploreButton.jsx
+++ src/components/ExploreButton.jsx
@@ -1,34 +1,32 @@
-import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const ExploreButton = ({ to, children, onClick }) => {
-  const navigate = useNavigate();
-
-  const handleListPageNavigate = () => {
-    if (onClick) onClick();
-    navigate(to);
-  };
-
-  return <StyledButton to={to} onClick={handleListPageNavigate}>
-    {children}
-  </StyledButton>;
+const ExploreButton = ({ to = "/list", children = "구경해보기" }) => {
+  return (
+    <StyledButtonLink to={to}>
+      {children}
+    </StyledButtonLink>
+  );
 };
 
-const StyledButton = styled(Link)`
+const StyledButtonLink = styled(Link)`
   display: flex;
   justify-content: center;
   align-items: center;
+  
   width: 280px;
   height: 56px;
   padding: 14px 24px;
+  margin: 24px auto; 
   
   background-color: var(--purple-600);
   border-radius: 12px;
   
   font: var(--font-18-bold);
   color: var(--white);
-  text-decoration: none; // Link 태그의 기본 밑줄 제거
+  line-height: 28px;
+  letter-spacing: -0.01em;
+  text-decoration: none; 
   
   cursor: pointer;
   transition: all 0.2s ease-in-out;
@@ -39,7 +37,6 @@ const StyledButton = styled(Link)`
 
   &:active {
     background-color: var(--purple-800);
-    transform: translateY(0);
   }
 `;
 
