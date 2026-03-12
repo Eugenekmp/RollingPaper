94370db3c18b714d4f901a310f8219f0457e4120
kujihye <gji135@naver.com>
2026-03-12 16:47:44 +0900

    style: 미디어 쿼리 수정

--- src/styles/media.js
+++ src/styles/media.js
@@ -1,9 +1,9 @@
 const size = {
-  mobile: '768px',
-  tablet: '1200px',
-}
+  mobile: "768px",
+  tablet: "1024px",
+};
 
 export const device = {
-  mobile: `media screen and (max-width: ${size.mobile}`,
-  tablet: `media screen and (max-width: ${size.tablet}`,
-}
+  mobile: `(max-width: ${size.mobile})`,
+  tablet: `(max-width: ${size.tablet})`,
+};
