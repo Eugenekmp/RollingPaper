74c03c289377309505780ade7cff6c81b7029f31
kujihye <gji135@naver.com>
2026-03-12 16:30:56 +0900

    feat: ThemeProvider를 사용해서 미디어 쿼리를 전체에 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import MainHeader from "./components/MainHeader";
 import MainPage from "./pages/MainPage";
 import Post from "./pages/Post";
@@ -8,24 +9,27 @@ import MessagePage from "./pages/MessagePage";
 import BackButton from "./components/BackButton";
 import GlobalStyle from "./styles/GlobalStyle";
 import SearchPage from "./pages/SearchPage";
+import { device } from "./styles/media";
 
-const App = () => {
+function App() {
   return (
     <>
-      <GlobalStyle />
-      <Router>
-        <MainHeader />
-        <BackButton />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/post" element={<Post />} />
-          <Route path="/post/:id" element={<DetailPage />} />
-          <Route path="/post/:id/edit" element={<DetailPage />} />
-          <Route path="/post/:id/message" element={<MessagePage />} />
-          <Route path="/list" element={<ListPage />} />
-          <Route path="/search" element={<SearchPage />} />
-        </Routes>
-      </Router>
+      <ThemeProvider theme={device}>
+        <GlobalStyle />
+        <Router>
+          <MainHeader />
+          <BackButton />
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/post" element={<Post />} />
+            <Route path="/post/:id" element={<DetailPage />} />
+            <Route path="/post/:id/edit" element={<DetailPage />} />
+            <Route path="/post/:id/message" element={<MessagePage />} />
+            <Route path="/list" element={<ListPage />} />
+            <Route path="/search" element={<SearchPage />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </>
   );
 };
